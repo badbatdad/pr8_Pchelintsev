5bdf40a68ac9749d1760ead019af76aba877da2a
badbatdad <filanto637@yandex.ru>
2025-05-22 01:11:01 +0300

    new changes

--- password_generator.py
+++ password_generator.py
@@ -2,8 +2,13 @@ import random
 import string
 
 def generate_password(length=10):
+<<<<<<< HEAD
     # Алфавит: строчные + заглавные буквы
     alphabet = string.ascii_letters
+=======
+    # Алфавит: строчные, заглавные, цифры и спецсимволы
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+>>>>>>> dev
     password = ''.join(random.choice(alphabet) for _ in range(length))
     return password
 
